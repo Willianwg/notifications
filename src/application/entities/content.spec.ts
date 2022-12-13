b50e0a1d3a155782a1867d4ceeff146a8f06254c
Willian Guedes <willian.wg70@gmail.com>
2022-12-13 23:21:35 +0000

    chore: format notification test

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,18 +1,20 @@
 import { Content } from "./content"
 
-test("it should be able to create a notification content", ()=>{
-    const content = new Content("Você recebeu uma solicitação de amizade!");
-
-    expect(content).toBeTruthy();
-    expect(content.value).toBe("Você recebeu uma solicitação de amizade!");
-})
-
-test("it should not be able to create a notification content with less than 5 characters", ()=>{
-
-    expect(()=> new Content("yes")).toThrow();
-})
-
-test("it should not be able to create a notification content with more than 240 characters", ()=>{
-
-    expect(()=> new Content("a".repeat(241))).toThrow();
+describe("Notification content", ()=>{
+    it("Should be able to create a notification content", ()=>{
+        const content = new Content("Você recebeu uma solicitação de amizade!");
+    
+        expect(content).toBeTruthy();
+        expect(content.value).toBe("Você recebeu uma solicitação de amizade!");
+    })
+    
+    it("Should not be able to create a notification content with less than 5 characters", ()=>{
+    
+        expect(()=> new Content("yes")).toThrow();
+    })
+    
+    it("Should not be able to create a notification content with more than 240 characters", ()=>{
+    
+        expect(()=> new Content("a".repeat(241))).toThrow();
+    })
 })
